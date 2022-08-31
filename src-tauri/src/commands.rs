d8241a548640a72daef98567d578ea726206f95d
Rodrez <fabian.rodrez@gmail.com>
2022-08-30 21:05:53 -0500

    Naive release. Functionality only works as happy path adjusted interface and rust commands are working

--- src-tauri/src/commands.rs
+++ src-tauri/src/commands.rs
@@ -1,28 +1,17 @@
-use std::fs;
+use std::{fs, path::PathBuf};
 use tauri::command;
-
-pub fn rename_file(file: &str, old_pattern: &str, new_pattern: &str) -> std::io::Result<()> {
-    fs::rename(&file, &file.replace(old_pattern, &new_pattern))?;
-    Ok(())
-}
 
 #[command]
 pub fn bulk_rename(directory: &str, old_pattern: &str, new_pattern: &str) -> String {
     let paths = fs::read_dir(&directory).unwrap();
 
-    for dir_entry in paths {
-        let file_name = dir_entry
-            .as_ref()
-            .unwrap()
-            .path()
-            .file_name()
-            .unwrap()
-            .to_string_lossy()
-            .into_owned();
-
-        if file_name.clone().contains(&old_pattern) {
-            rename_file(&file_name, &old_pattern, &new_pattern).unwrap();
-        }
+    for path in paths {
+        let path = path.unwrap();
+        let file_name = path.file_name().into_string().unwrap();
+        let new_name = file_name.replace(old_pattern, new_pattern);
+        let new_path = PathBuf::from(&directory).join(new_name);
+        fs::rename(path.path(), new_path).unwrap();
     }
-    format!("Bulk renaming completed.")
+
+    "Completed!".to_string()
 }
--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import { invoke } from "@tauri-apps/api";
 
 function App() {
   const [directory, setDirectory] = useState("");
-  const [oldPattern, setOldPattern] = useState("copy");
-  const [newPattern, setNewPattern] = useState("non copy");
+  const [oldPattern, setOldPattern] = useState("ABC");
+  const [newPattern, setNewPattern] = useState("DEF");
   const [completeMessage, setCompleteMessage] = useState("");
 
   async function getFiles() {
@@ -21,6 +21,7 @@ function App() {
 
   // Rust renamer
   function rename() {
+    setCompleteMessage("Renaming the files.");
     invoke("bulk_rename", {
       directory: directory,
       oldPattern: oldPattern,
@@ -39,36 +40,74 @@ function App() {
           />
         </a>
       </div>
-      <h2 className="uppercase text-3xl font-semibold">Rustnamer</h2>
-      <p className="read-the-docs">
-        Select a directory a update an old pattern with a new one.
-      </p>
-      <div className="flex w-full gap-x-4">
-        <div className="text-left">
-          <p>Old Pattern</p>
+      <h2 className="uppercase text-3xl font-semibold text-center">
+        Rustnamer
+      </h2>
+
+      <div>
+        <p className="read-the-docs">
+          Select a directory a update an old pattern with a new one.
+        </p>
+
+        <button
+          onClick={getFiles}
+          className="px-5 hover:scale-[1.02] transition-transform shadow-md py-2.5 w-full text-center text-lg font-bold text-white bg-gradient-to-br from-[#383f6b] to-[#141727] rounded-lg"
+        >
+          Select Folder
+        </button>
+        <p className="text-sm mb-4 mt-1 read-the-docs">
+          Selected:
+          <span className=" text-green-500">{directory}</span>
+        </p>
+        <div className="mb-6">
+          <label
+            htmlFor="old_pattern"
+            className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300"
+          >
+            Old Pattern
+          </label>
           <input
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:outline-amber-500 focus:ring-amber-500 focus:border-ambers-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500"
+            id="old_pattern"
+            className="bg-transparent border-2 border-gray-600 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-amber-600 focus:outline-amber-900 focus:border-amber-400 block w-full p-2.5 dark:text-white"
+            placeholder="ABC"
             onChange={(e) => setOldPattern(e.target.value)}
             value={oldPattern}
           />
         </div>
-        <div className="text-left">
-          <p>New Pattern</p>
+        <div className="mb-6">
+          <label
+            htmlFor="new_pattern"
+            className="block mb-2 text-lg font-medium text-gray-900 dark:text-gray-300"
+          >
+            New Pattern
+          </label>
           <input
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-amber-500 focus:border-amber-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-amber-500 dark:focus:border-amber-500"
+            id="new_pattern"
+            className="bg-transparent border-2 border-gray-600 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-amber-600 focus:outline-slate-900 focus:border-amber-300 block w-full p-2.5 dark:text-white"
+            placeholder="DEF"
             onChange={(e) => setNewPattern(e.target.value)}
             value={newPattern}
           />
         </div>
+        <button
+          onClick={rename}
+          disabled={!directory}
+          className="w-full text-white bg-gradient-to-br from-[#ec9f05] to-[#ff4e00] rounded-lg text-lg font-semibold px-5 py-2.5 mb-2 items-center shadow-md shadow-gray-300 text-center dark:shadow-gray-900 hover:scale-[1.02] transition-transform"
+        >
+          RENAME
+        </button>
+        <div className="mt-12">
+          {completeMessage}
+          <div className="overflow-hidden rounded-full bg-slate-800 ">
+            <div
+              className="h-2 rounded-full bg-green-600 transform"
+              style={{
+                width: completeMessage.includes("Completed") ? "100%" : "0%",
+              }}
+            />
+          </div>
+        </div>
       </div>
-      <button onClick={getFiles}>Select Folder</button>
-      <div>
-        <p>{directory}</p>
-        Status: {completeMessage}
-      </div>
-      <button className="bg-amber-500 text-slate-800" onClick={rename}>
-        Rename
-      </button>
     </div>
   );
 }
