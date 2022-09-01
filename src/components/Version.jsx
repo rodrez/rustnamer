23c24d349b42538ae26bd772c2146e465c453338
Rodrez <fabian.rodrez@gmail.com>
2022-09-01 11:11:09 -0500

    Created work flow to automatically create releases

--- src/components/Version.jsx
+++ src/components/Version.jsx
@@ -1,7 +1,7 @@
 export default function Version({ version }) {
   return (
     <div className="absolute bottom-2 right-2 text-xs text-gray-600">
-      v. {version}
+      v{version}
     </div>
   );
 }
